6c89db20921dd1378b900ae0706f0edb4a5946f2
sy0419 <psy041912@gmail.com>
2025-06-18 23:54:36 +0900

    Switch uppercase and lowercase letters in the given string.

--- level0/004_switchUpperLower.js
+++ level0/004_switchUpperLower.js
@@ -26,3 +26,32 @@ r1.on('line', function(line) {
     }
     console.log(result);
 })
+
+// }).on('close', function() {
+//     let result = '';
+// 변환된 문자를 하나씩 모을 빈 문자열 변수입니다.
+// 처음에는 아무 것도 없고, 한 글자씩 대소문자를 바꿔서 여기다가 이어 붙이게 됩니다.
+
+//     for(let i = 0; i < input.length; i++) {
+// 용자 입력으로 받은 문자열이 input에 배열로 저장되어 있으므로, 배열의 길이만큼 반복문을 돌면서 한 글자씩 처리합니다.
+
+//         const str = input[i];
+// input 배열에서 현재 인덱스의 문자를 꺼내서 str에 저장합니다.
+// 예: input이 ['a', 'B', 'c']이면, i=0 → str = 'a', i=1 → str = 'B', i=2 → str = 'c'
+
+//         if (str == str.toUpperCase()) {
+// 현재 문자가 대문자인지 검사합니다. 즉, str.toUpperCase()는 문자를 대문자로 바꾼 결과입니다.
+// 만약 원래 문자와 대문자로 바꾼 값이 같다면 → 원래도 대문자
+
+//             result += str.toLowerCase();
+// 대문자였으면 → 소문자로 바꿔서 result에 추가합니다.
+
+//         } else {
+//             result += str.toUpperCase();
+//만약 대문자가 아니라면 소문자이므로 대문자로 바꿔서 result에 추가합니다.
+
+//         }
+//     }
+//     console.log(result);
+// })
+// 변환이 완료된 최종 결과 문자열을 콘솔에 출력합니다.
