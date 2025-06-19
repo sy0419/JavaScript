adda718f952a363d13d7d037d00288a278b306df
sy0419 <psy041912@gmail.com>
2025-06-19 15:27:53 +0900

    Print symbols.

---
+++ level0/005_printSymbols.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = []
+
+r1.on('line', function(line) {
+    input = line.split('');
+    r1.close();
+}).on('close', function() {
+    const str = input.join('');
+    console.log(str);  
+})
