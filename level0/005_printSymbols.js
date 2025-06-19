ca297651ac9c784e89424cac9d630ebe8b7b2d30
sy0419 <psy041912@gmail.com>
2025-06-19 15:56:55 +0900

    Print symbols.

--- level0/005_printSymbols.js
+++ level0/005_printSymbols.js
@@ -1,14 +1,14 @@
 const readline = require('readline');
-const r1 = readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 let input = []
 
-r1.on('line', function(line) {
+rl.on('line', function(line) {
     input = line.split('');
-    r1.close();
+    rl.close();
 }).on('close', function() {
     const str = input.join('');
     console.log(str);  
