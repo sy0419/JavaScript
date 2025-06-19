f41ba138da1bff571183727d459441ce7e113728
sy0419 <psy041912@gmail.com>
2025-06-19 15:46:46 +0900

    Print given string as a line without spaces.

---
+++ level0/007_printStringConsecutively.js
@@ -0,0 +1,20 @@
+// 두 개의 문자열 str1, str2가 공백으로 구분되어 입력으로 주어집니다.
+// 입출력 예와 같이 str1과 str2을 이어서 출력하는 코드를 작성해 보세요.
+// 제한사항) 1 ≤ str1, str2의 길이 ≤ 10
+
+const readline = require('readline');
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = []
+
+r1.on('line', function (line) {
+    input = line.split(' ');
+    r1.close();
+}).on('close', function() {
+    const str1 = input[0];
+    const str2 = input[1];
+    console.log(str1 + str2);
+});
