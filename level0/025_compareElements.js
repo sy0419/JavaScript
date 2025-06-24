99c210af241ef35096dbc72db3eb350ab88cfc5f
sy0419 <psy041912@gmail.com>
2025-06-25 00:54:31 +0900

    Compare the last two elements, and append either their difference or double of the last element depending on the condition.

--- level0/025_compareElements.js
+++ level0/025_compareElements.js
@@ -4,6 +4,17 @@
 // 2 ≤ num_list의 길이 ≤ 10
 // 1 ≤ num_list의 원소 ≤ 9
 
+function solution(num_list) {
+    if(num_list[num_list.length - 1] > num_list[num_list.length -2]) {
+        num_list.push(num_list[num_list.length - 1] - num_list[num_list.length -2])
+    } else {
+        num_list.push(num_list[num_list.length - 1] * 2)
+    }
+    return num_list
+};
+console.log(solution([2, 1, 6]));
+console.log(solution([5, 2, 1, 7, 5]));
+
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
