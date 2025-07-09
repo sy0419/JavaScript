0b1b2a4248db9cee3662d15cac26986d8f83f06e
sy0419 <psy041912@gmail.com>
2025-07-09 23:21:04 +0900

    Extract letters from the given string using the specified indexes.

--- level0/037_makeWord.js
+++ level0/037_makeWord.js
@@ -9,6 +9,7 @@ function solution(my_string, index_list) {
     return index_list.map(i => my_string[i]).join('');
 }
 console.log(solution("cvsgiorszzzmrpaqpe", [16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7]));
+console.log(solution("zpiaz", [1, 2, 0, 0, 3]));
 
 const readline = require('readline');
 const rl = readline.createInterface({
